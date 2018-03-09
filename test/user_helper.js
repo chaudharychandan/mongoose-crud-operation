b41e0fe6b8ca9a8111938c7c5d8858cb3a30269b
Chandan kumar <chandanchaudharyrules@gmail.com>
2018-03-10 01:30:31 +0530

    Add Before each method call which runs before every test case and drops users collection

--- test/user_helper.js
+++ test/user_helper.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
+//mongodb connection using mongoose ORM
 mongoose.connect('mongodb://localhost/users_test');
-
 mongoose.connection
   .once('open', () => console.log('Connected to users_test DB'))
   .on('error', (error) => console.warn('Warn', error))
   ;
+
+//Runs before every test case
+beforeEach((done) => {
+  //Drops users collection before every it block
+  //Invokes done callback function once drop operation completes
+  mongoose.connection.collections.users.drop(() => {
+    done();
+  })
+});
