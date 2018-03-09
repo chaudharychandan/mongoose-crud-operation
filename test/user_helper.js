f0a108ebf86a08797f9aafb630f1444dc2c47e7d
Chandan kumar <chandanchaudharyrules@gmail.com>
2018-03-10 01:48:34 +0530

    Move connection string in before block and invoke done callback done once connection establishes Close database connection after all the test case completes

--- test/user_helper.js
+++ test/user_helper.js
@@ -1,17 +1,26 @@
 const mongoose = require('mongoose');
 
-//mongodb connection using mongoose ORM
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', () => console.log('Connected to users_test DB'))
-  .on('error', (error) => console.warn('Warn', error))
-  ;
+before((done) => {
+  //mongodb connection using mongoose ORM
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => console.warn('Warn', error))
+    ;
+});
 
 //Runs before every test case
 beforeEach((done) => {
   //Drops users collection before every it block
   //Invokes done callback function once drop operation completes
   mongoose.connection.collections.users.drop(() => {
+    done();
+  })
+});
+
+after((done) => {
+  //Close database connection
+  mongoose.connection.close(() => {
     done();
   })
 });
